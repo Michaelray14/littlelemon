f8b6b8828722cb113b481fe9835a3257ffad26d7
michaelray14 <mikeray14@gmail.com>
2023-06-30 11:34:20 -0500

    new updates to the branch with apps

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o-!npq3lp_vjkw#i$um26^l#sk-_&&p^=#wi0n+d7sm3t&wa1-'
+SECRET_KEY = 'django-insecure-wpbap=5=8rmk+$9rv9oi05w#+#!3d+fxbv)c76=kiec*!h_xiv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.reservation',
+    'restaurant'
 ]
 
 MIDDLEWARE = [
