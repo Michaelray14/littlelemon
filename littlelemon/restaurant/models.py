d8d14f2bb7e6eeaaaed3094e126824a021f4bfcb
michaelray14 <mikeray14@gmail.com>
2023-07-02 21:11:48 -0500

    added the API for table booking and the menu

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    item = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Booking(models.Model):
+    customer_name = models.CharField(max_length=200)
+    booking_date = models.DateField()
+    booking_time = models.TimeField()
+    table_number = models.IntegerField()
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .views import sayHello
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     ##path('', sayHello, name='sayHello'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view()),
+    path('', include(router.urls)),
 ]
